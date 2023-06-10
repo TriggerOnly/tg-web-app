758a152222d53d2c2f6665b9c33cde3010449615
TriggerOnly <127595188+TriggerOnly@users.noreply.github.com>
2023-06-10 16:01:49 +0400

    debug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
+import Header from './components/Header/Header'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Form from './components/Form/Form';
+import ProductList from './components/ProductList/ProductList';
 
 function App() {
+  const {onToggleButton, tg} = useTelegram()
+
+  useEffect(() => { 
+    tg.ready()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path='/' element={<ProductList/>}/>
+          <Route path='/form' element={<Form/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
